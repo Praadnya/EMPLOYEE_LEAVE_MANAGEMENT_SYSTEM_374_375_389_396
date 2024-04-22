8c7cab1ad8188ae9b811413a0c25f26d20093573
QubitMatrix <preethi13m@gmail.com>
2024-04-22 23:24:41 +0530

    Updated leaveHistory service to implement manager leave history view

--- src/main/java/com/example/demo/service/LeaveHistoryService.java
+++ src/main/java/com/example/demo/service/LeaveHistoryService.java
@@ -2,11 +2,13 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import com.example.demo.entity.DepartmentEnum;
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.LeaveHistory;
 
 public interface LeaveHistoryService {
     List<LeaveHistory> getAllLeaveHistories(Employee employee);
+    List<Employee> getAllUsernames(DepartmentEnum deptname);
     // Other service methods if needed
 }
 
--- src/main/java/com/example/demo/service/LeaveHistoryServiceImpl.java
+++ src/main/java/com/example/demo/service/LeaveHistoryServiceImpl.java
@@ -5,19 +5,29 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.DepartmentEnum;
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.LeaveHistory;
+import com.example.demo.repo.EmployeeRepo;
 import com.example.demo.repo.LeaveHistoryRepository;
 
 @Service
 public class LeaveHistoryServiceImpl implements LeaveHistoryService {
 
     @Autowired
+    private EmployeeRepo employeeRepository;
+    
+    @Autowired
     private LeaveHistoryRepository leaveHistoryRepository;
 
     @Override
     public List<LeaveHistory> getAllLeaveHistories(Employee employee) {
         return leaveHistoryRepository.findByEmployee(employee);
+    }
+
+    @Override
+    public List<Employee> getAllUsernames(DepartmentEnum deptname) {
+        return employeeRepository.findByDeptName(deptname);
     }
 
 	    
